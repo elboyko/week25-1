7546b2b7b59adaec7bad158919a230490f0607d6
elboyko <boyko.lena@gmail.com>
2024-03-14 09:49:17 +0700

    add task

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './components/Counter'
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Click</h1>
+      <Counter />
+    </div>)
+};
+export default App;
